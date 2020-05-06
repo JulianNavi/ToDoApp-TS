f80739c1f70bd442b6d6fe3b73e2bee0bc7afac6
JulianNavi <39164188+JulianNavi@users.noreply.github.com>
2020-05-06 20:38:27 +0300

    define the data model and create the todo item collection class

---
+++ src/toDoCollections.ts
@@ -0,0 +1,28 @@
+import { ToDoItem } from './toDoItem'
+
+export class ToDoCollection {
+  private nextId: number = 1;
+  constructor(
+    public userName: string,
+    public todoItems: ToDoItem[] = []) {
+
+  }
+  addToDo(task: string): number {
+    while (this.getToDoById(this.nextId)) {
+      this.nextId++
+    }
+    this.todoItems.push(new ToDoItem(this.nextId, task))
+    return this.nextId
+  }
+  getToDoById(id: number): ToDoItem {
+    return this.todoItems.find(item => item.id === id)
+  }
+  markComplete(
+    id: number,
+    complete: boolean) {
+    const todoItem = this.getToDoById(id)
+    if (todoItem) {
+      todoItem.complete = complete
+    }
+  }
+}
